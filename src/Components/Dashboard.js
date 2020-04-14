6806d78196fd963db81a40783f665c2495e9a3e6
SohamChhapre <1899sohamchhapre@gmail.com>
2020-04-14 12:26:21 +0530

    Navigation of icons on navbar

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import payment_icon from './../Icons/Icons-Footer/payment.png';
 import home_icon from './../Icons/Icons-Footer/home.png';
 import support_icon from './../Icons/Icons-Footer/online_support.png';
@@ -7,7 +7,13 @@ import default_home from './../Icons/Icons-Footer/home_default.png';
 import default_payment from './../Icons/Icons-Footer/payment_default.png';
 import default_support from './../Icons/Icons-Footer/support_default.png';
 import default_food_delivery from './../Icons/Icons-Footer/food_delivery_default.png';
+const defaultState={
+    "Home":false,
+    "Orders":false,
+    "Payments":false,
+    "Support":false
 
+}
 
 const NavActiveicon=({img,name})=>{
 
@@ -16,17 +22,24 @@ const NavActiveicon=({img,name})=>{
     )
 }
 
-const NavIcon=({img,name})=>{
+const NavIcon=({img,name,setActive})=>{
 
     return (
-        <div className="text-center pt-1"> <img src={img} className="nav-icon" alt="home" /><p className="mb-1 nav-p-sty" style={{color:"#b8bcc7",fontFamily:"Poppins-SemiBold"}}>{name}</p></div>
+        <div className="text-center pt-1" onClick={()=>{
+            setActive({...defaultState,[name]:true})
+        }}> <img src={img} className="nav-icon" alt="home" /><p className="mb-1 nav-p-sty" style={{color:"#b8bcc7",fontFamily:"Poppins-SemiBold"}}>{name}</p></div>
     )
 }
 
 
 const Dashboard=()=>{
-
-
+    const [active,setActive]=useState({'Home':true,"Payments":false,"Orders":false,"Support":false})
+    useEffect(()=>{
+        setActive({...defaultState,"Home":true})
+    },[])
+    useEffect(()=>{
+            console.log(10);
+    },[active])
     return(
             <div className="main-container" >
             
@@ -61,16 +74,14 @@ const Dashboard=()=>{
 
             
             
-            <nav class="navbar fixed-bottom dash-nav" style={{borderRadius:"0px 0px 18px 18px",padding:"0px 1rem 0rem 1rem"}}>
-               {/* <div className="text-center pt-1"> <img src={home_icon} className="nav-icon" alt="home" /><p className="mb-1">Home</p></div>
-                <div className="text-center pt-1"><img src={payment_icon}  className="nav-icon" alt="payment" /><p className="mb-1">Payments</p></div>
-                <div className="text-center pt-1"><img src={food_icon} className="nav-icon" alt="food" /><p className="mb-1">Orders</p></div>
-                <div className="text-center pt-1"><img src={support_icon} className="nav-icon mx-auto" alt="Support"/><p className="mb-1">Support</p></div> */}
-                {/* <NavIcon img={default_home} name={"Home"}/> */}
-                <NavActiveicon img={home_icon} name={"Home"}/>
-                <NavIcon img={default_payment} name={"Payments"}/>
-                <NavIcon img={default_food_delivery} name={"Orders"}/>
-                <NavIcon img={default_support} name={"Support"}/>
+            <nav className="navbar fixed-bottom dash-nav" style={{borderRadius:"0px 0px 18px 18px",padding:"0px 1rem 0rem 1rem"}}>
+               
+
+                { active["Home"]?(<NavActiveicon img={home_icon} name={"Home"}/>):(<NavIcon img={default_home} name={"Home"} setActive={setActive}/>)}
+                { active["Payments"]?(<NavActiveicon img={payment_icon} name={"Payments"}/>):(<NavIcon img={default_payment} name={"Payments"} setActive={setActive}/>)}
+                { active["Orders"]?(<NavActiveicon img={food_icon} name={"Orders"}/>):(<NavIcon img={default_food_delivery} name={"Orders"} setActive={setActive}/>)}
+                { active["Support"]?(<NavActiveicon img={support_icon} name={"Support"}/>):(<NavIcon img={default_support} name={"Support"} setActive={setActive}/>)}
+            
             
             </nav> 
 
