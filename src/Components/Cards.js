bbe79f5544e201ca726ee3288dfdc90d4a9fbae1
SohamChhapre <1899sohamchhapre@gmail.com>
2020-04-15 11:25:55 +0530

    Food page impl-1

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -6,8 +6,43 @@ const Cards=()=>{
 
     return (
 
-        <div className="main-container">
+        <div className="main-container" style={{height:"calc(100vh - 100px)",overflowY:"scroll",padding:"0px 5%",marginLeft:"0px",marginRight:"0px"}}>
+
+
+        <div className="card mx-auto dash-banner" style={{backgroundColor:"#E5F5EE",border:"0px",borderRadius:'5px'}}>
+            
+            </div>
+
+
+        
          <div className="flex-container" >
+         
+            <div className="food-icon-card" >
+            </div>
+            <div className="food-icon-card mg-left" >
+            </div>
+            </div>
+        <p style={{margin:"20px 0px 0px 0px"}} className="mt-5"> Category-1</p>
+        <div className="flex-container my-0" >
+           
+            <div className="horizontal-card" >
+            </div>
+             <div className="horizontal-card" >
+            </div>
+        </div>
+        <p style={{margin:"20px 0px 0px 0px"}} className="mt-5"> Category-2</p>
+            <div className="flex-container my-0" >
+           
+            <div className="horizontal-card" >
+            </div>
+             <div className="horizontal-card" >
+            </div>
+             <div className="horizontal-card" >
+            </div>
+             <div className="horizontal-card" >
+            </div>
+        </div>
+            {/* <div className="flex-container">
             <div className="icon-card" >
             </div>
             <div className="icon-card mg-left" >
@@ -24,19 +59,13 @@ const Cards=()=>{
             </div>
             <div className="icon-card mg-left" >
             </div>
-            </div>
-            <div className="flex-container">
+            </div> */}
+            {/* <div className="flex-container" >
             <div className="icon-card" >
             </div>
             <div className="icon-card mg-left" >
             </div>
-            </div>
-            <div className="flex-container">
-            <div className="icon-card" >
-            </div>
-            <div className="icon-card mg-left" >
-            </div>
-            </div>
+            </div> */}
         </div>
     )
 }
