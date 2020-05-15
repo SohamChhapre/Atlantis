b2964e153afede98da302dc60a121f0d632f22d0
SohamChhapre <1899sohamchhapre@gmail.com>
2020-05-15 17:11:17 +0530

    Laundry icon changes

--- src/Components/Redux/Laundary/LaundaryReducer.js
+++ src/Components/Redux/Laundary/LaundaryReducer.js
@@ -2,10 +2,13 @@ import {INC_LAUNDRY,DEC_LAUNDRY} from './LaundaryTypes.js'
 
 
 const initialState={
-    LaundryInit:[{id:1,name:"Iron & Fold",items:0,category:"Casual",url:"https://firebasestorage.googleapis.com/v0/b/rumakita.appspot.com/o/Icons%26Illustrations%2Fic-ironandfold(1).png?alt=media&token=12b6189b-9b76-47fd-a7e7-030c1c5f189c"},
+    LaundryInit:[
     {id:2,name:"Wash & Fold",items:0,category:"Casual",url:"https://firebasestorage.googleapis.com/v0/b/rumakita.appspot.com/o/Icons%26Illustrations%2Fimg-laundry(1).png?alt=media&token=9260c795-4b14-4b76-9235-fed399349fb7"},
-    {id:3,name:"Iron & Fold",items:0,category:"Formal",url:"https://firebasestorage.googleapis.com/v0/b/rumakita.appspot.com/o/Icons%26Illustrations%2Fic-ironandfold(1).png?alt=media&token=12b6189b-9b76-47fd-a7e7-030c1c5f189c"},
-    {id:4,name:"Wash & Fold",items:0,category:"Formal",url:"https://firebasestorage.googleapis.com/v0/b/rumakita.appspot.com/o/Icons%26Illustrations%2Fimg-laundry(1).png?alt=media&token=9260c795-4b14-4b76-9235-fed399349fb7"}]
+        
+        {id:2,name:"Iron & Fold",items:0,category:"Casual",url:"https://firebasestorage.googleapis.com/v0/b/rumakita.appspot.com/o/Icons%26Illustrations%2Fic-ironandfold(1).png?alt=media&token=12b6189b-9b76-47fd-a7e7-030c1c5f189c"},
+    {id:3,name:"Wash & Fold",items:0,category:"Formal",url:"https://firebasestorage.googleapis.com/v0/b/rumakita.appspot.com/o/Icons%26Illustrations%2Fimg-laundry(1).png?alt=media&token=9260c795-4b14-4b76-9235-fed399349fb7"},
+    {id:4,name:"Iron & Fold",items:0,category:"Formal",url:"https://firebasestorage.googleapis.com/v0/b/rumakita.appspot.com/o/Icons%26Illustrations%2Fic-ironandfold(1).png?alt=media&token=12b6189b-9b76-47fd-a7e7-030c1c5f189c"}
+    ]
 }
 
 const LaundaryReducer=(state=initialState,action)=>{
